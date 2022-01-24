8743785b74f0a391fbda777b6a4d75dbbd89cd84
bramstorp <bramstorpmathias@gmail.com>
2022-01-24 10:50:48 +0100

    added get for single and multiple cars

--- server-side/backendfastapi/app/routers/cars.py
+++ server-side/backendfastapi/app/routers/cars.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
+from typing import List
 
 from ..models.cars import Car
 from ..schemas.cars import CarIn_Pydantic, Car_Pydantic
@@ -16,3 +17,11 @@ router = APIRouter(
 async def create_car(car: CarIn_Pydantic):
     car_obj = await Car.create(**car.dict(exclude_unset=True))
     return await Car_Pydantic.from_tortoise_orm(car_obj)
+
+@router.get('/cars', response_model=List[Car_Pydantic])
+async def get_cars():
+    return await Car_Pydantic.from_queryset(Car.all())  
+
+@router.get('/car/{car_id}', response_model=Car_Pydantic)
+async def get_car(car_id: int):
+    return await Car_Pydantic.from_queryset_single(Car.get(id=car_id))
