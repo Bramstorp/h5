1025d4b9371ae6a1ce17b87b74e34555e46a398f
bramstorp <bramstorpmathias@gmail.com>
2022-01-24 09:37:57 +0100

    added base main file

---
+++ server-side/backend/app/__init__.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise import Tortoise
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return "Home"
+
+
+Tortoise.init_models([], 'models')
+
+register_tortoise(
+    app, 
+    db_url='sqlite://db.sqlite3',
+    modules={
+        'models': [],
+    },
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
