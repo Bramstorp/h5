2938cf8faec57b9aae33a65e56d8777120fca9dc
Bramstorp <m.bramstorp@stofanet.dk>
2022-01-25 11:01:58 +0100

    added base design for car wash

--- server-side/frontend/src/App.js
+++ server-side/frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+import { NotFound } from "./NotFound";
+import { Layout } from "./layout/layout";
+
+import { Home } from "./modules/Home/Home"
+
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+    return ( 
+      <BrowserRouter basename={"/"}>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    );
 }
 
 export default App;
