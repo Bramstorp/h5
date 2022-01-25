313cc034f3167718edac166a0a447ab067272023
Bramstorp <m.bramstorp@stofanet.dk>
2022-01-25 14:39:00 +0100

    added start, stop, free carwash

--- server-side/backendfastapi/app/routers/carwashes.py
+++ server-side/backendfastapi/app/routers/carwashes.py
@@ -16,13 +16,13 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-#Create carwash
+# Create carwash
 @router.post('/carwash', response_model=Carwash_Pydantic)
 async def create_carwash(carwash: CarwashIn_Pydantic):
     carwash_obj = await Carwash.create(**carwash.dict(exclude_unset=True))
     return await Carwash_Pydantic.from_tortoise_orm(carwash_obj)
 
-#Get carwash from id
+# Get carwash from id
 @router.get('/carwash/{carwash_id}', response_model=Carwash_Pydantic)
 async def get_carwash(carwash_id: int):
     return await Carwash.get(id=carwash_id)
@@ -32,19 +32,31 @@ async def get_carwash(carwash_id: int):
 async def get_carwash(carwash_name: str):
     return await Carwash.get(name=carwash_name)
 
-#Get all carwashes
+# Get all carwashes
 @router.get('/carwashes', response_model=List[Carwash_Pydantic])
 async def get_carwashes():
     return await Carwash_Pydantic.from_queryset(Carwash.all())  
 
-#Pause carwash
+# Rename carwash
 @router.put('/carwash/', response_model=Carwash_Pydantic)
-async def update_carwash(carwash_id: int, carwash_status: str):
-    await Carwash.filter(id=carwash_id).update({"status": carwash_status})
+async def update_carwash(carwash_id: int, carwash_name: str):
+    await Carwash.filter(id=carwash_id).update(name=carwash_name)
     return await Carwash_Pydantic.from_queryset_single(Carwash.get(id=carwash_id))
 
-#Start carwash
-@router.put('/carwash/{carwash_id}', response_model=Carwash_Pydantic)
-async def get_carwash(carwash_id: int):
-    return await Carwash.get(id=carwash_id)
+# Start carwash
+@router.put('/carwash/start/{carwash_id}', response_model=Carwash_Pydantic)
+async def start_carwash(carwash_id: int):
+    await Carwash.filter(id=carwash_id).update(status="RUNNING")
+    return await Carwash_Pydantic.from_queryset_single(Carwash.get(id=carwash_id))
 
+# stop carwash
+@router.put('/carwash/stop/{carwash_id}', response_model=Carwash_Pydantic)
+async def stop_carwash(carwash_id: int):
+    await Carwash.filter(id=carwash_id).update(status="STOPPED")
+    return await Carwash_Pydantic.from_queryset_single(Carwash.get(id=carwash_id))
+
+# Open carwash
+@router.put('/carwash/running/{carwash_id}', response_model=Carwash_Pydantic)
+async def free_carwash(carwash_id: int):
+    await Carwash.filter(id=carwash_id).update(status="RUNNING")
+    return await Carwash_Pydantic.from_queryset_single(Carwash.get(id=carwash_id))
