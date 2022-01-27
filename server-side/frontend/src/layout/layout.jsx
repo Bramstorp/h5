c6d39596b1232339daf54148409d3a58c897afdc
Daniel Würtz Ankerstjerne <dwa@flexfone.dk>
2022-01-27 09:48:56 +0100

    Login styling

--- server-side/frontend/src/layout/layout.jsx
+++ server-side/frontend/src/layout/layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar } from "./navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export const Layout = (props) => (
   <div>
