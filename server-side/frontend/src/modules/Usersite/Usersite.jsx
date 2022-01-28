1b61f3f518d064c0a482bea148d259af096e7330
Daniel Würtz Ankerstjerne <dwa@flexfone.dk>
2022-01-28 09:38:43 +0100

    User data on userstite

--- server-side/frontend/src/modules/Usersite/Usersite.jsx
+++ server-side/frontend/src/modules/Usersite/Usersite.jsx
@@ -22,12 +22,16 @@ export const Usersite = () => {
           setUser(res)
         })
       }, [])
-      console.log(user)
   return (
 <div>     
 <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
     <div className="card p-4">
-        <div className=" image d-flex flex-column justify-content-center align-items-center"> <button className="btn btn-secondary"> <img src="https://thispersondoesnotexist.com/image" height="100" width="100" /></button> <span className="name mt-3">Name</span> <span className="subscription">Subscription active / inactive</span>
+        <div className=" image d-flex flex-column justify-content-center align-items-center"> <button className="btn btn-secondary"> <img src="https://thispersondoesnotexist.com/image" height="100" width="100" /></button> <span className="name mt-3">{user.name}</span> 
+        {user && user.is_subscribed ? (
+        <span className="subscription">Subscription active</span>
+      ) : (
+        <span className="subscription">Subscription inactive</span>
+      )}
             <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i className="fa fa-twitter"></i></span> <span><i className="fa fa-facebook-f"></i></span> <span><i className="fa fa-instagram"></i></span> <span><i className="fa fa-linkedin"></i></span> </div>
         </div>
     </div>
