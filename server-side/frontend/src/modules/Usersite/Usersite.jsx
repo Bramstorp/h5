9012955fd5c64029d61aa141e6adf548468111ff
Bramstorp <m.bramstorp@stofanet.dk>
2022-01-30 19:29:07 +0100

    added no washer check

--- server-side/frontend/src/modules/Usersite/Usersite.jsx
+++ server-side/frontend/src/modules/Usersite/Usersite.jsx
@@ -28,37 +28,55 @@ export const Usersite = () => {
       }, [])
   return (
     <>
-    {user ?
-      <div>     
-        <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
-          <div className="card p-4">
-              <div className=" image d-flex flex-column justify-content-center align-items-center"> 
-                <button className="btn btn-secondary"> 
-                  <img src="https://thispersondoesnotexist.com/image" height="100" width="100" />
+      {error ? <div className="alert alert-danger">{error}</div> : ""}
+      {user ? (
+        <div>
+          <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
+            <div className="card p-4">
+              <div className=" image d-flex flex-column justify-content-center align-items-center">
+                <button className="btn btn-secondary">
+                  <img
+                    src="https://thispersondoesnotexist.com/image"
+                    height="100"
+                    width="100"
+                  />
                 </button>
-                {user.is_admin ? 
-                  <span>
-                    ADMIN
-                  </span> : "" }
-                <span className="name">
-                  name: {user.username}
-                </span>
+                {user.is_admin ? <span>ADMIN</span> : ""}
+                <span className="name">name: {user.username}</span>
                 {user.is_subscribed ? (
                   <span className="subscription">Subscription active</span>
                 ) : (
                   <span className="subscription">Subscription inactive</span>
                 )}
-                <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center"> <span><i className="fa fa-twitter"></i></span> <span><i className="fa fa-facebook-f"></i></span> <span><i className="fa fa-instagram"></i></span> <span><i className="fa fa-linkedin"></i></span> </div>
+                <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
+                  {" "}
+                  <span>
+                    <i className="fa fa-twitter"></i>
+                  </span>{" "}
+                  <span>
+                    <i className="fa fa-facebook-f"></i>
+                  </span>{" "}
+                  <span>
+                    <i className="fa fa-instagram"></i>
+                  </span>{" "}
+                  <span>
+                    <i className="fa fa-linkedin"></i>
+                  </span>{" "}
+                </div>
               </div>
+            </div>
           </div>
+          {currentWash ? (
+            <div style={{ textAlign: "-webkit-center" }}>
+              <Wash admin={true} wash={currentWash} handleChange={fetchWash} />
+            </div>
+          ) : (
+            ""
+          )}
         </div>
-        {currentWash ? 
-        <div style={{ textAlign: "-webkit-center" }}>
-          <Wash admin={true} wash={currentWash} handleChange={fetchWash} />
-        </div>
-        : ""}
-      </div>
-    : ""}
+      ) : (
+        ""
+      )}
     </>
   );
 };
